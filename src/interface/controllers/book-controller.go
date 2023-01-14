ffaa49475343e2ff886d096b99983fd7bd6dbda7
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-01-14 21:03:42 +0100

    ✨ Book Controller [ADD method]

---
+++ src/interface/controllers/book-controller.go
@@ -0,0 +1,38 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/olusamimaths/go-book-application/src/domain"
+	"github.com/olusamimaths/go-book-application/src/usecases"
+)
+
+type BookController struct {
+	bookInteractor usecases.BookInteractor
+}
+
+func NewBookController(bookInteractor usecases.BookInteractor) *BookController {
+	return &BookController{bookInteractor}
+}
+
+func (controller *BookController) Add(res http.ResponseWriter, req http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+
+	var book domain.Book
+	err := json.NewDecoder(req.Body).Decode(&book)
+
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(res).Encode(ErrorResponse{Message: "Invalid Payload"})
+		return
+	}
+	
+	err = controller.bookInteractor.CreateBook(book)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(res).Encode(ErrorResponse{Message: err.Error()})
+		return
+	}
+	res.WriteHeader(http.StatusOK)
+}
